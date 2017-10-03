f0c9a02c30c1cb31d5e6f79a67ab3494bbc62c6d
David Grant <mrdavidgrant@outlook.com>
2017-10-03 11:04:31 -0400

    CurrentUser name showing up properly.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,30 @@ import ChatBar from './ChatBar.jsx'
 import NavBar from './NavBar.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
   render() {
     console.log("Rendering <App/>");
     return (
     <div>
       <NavBar />  
       <MessageList />
-      <ChatBar />
+      <ChatBar name = {this.state.currentUser.name} />
     </div>
     );
   }
