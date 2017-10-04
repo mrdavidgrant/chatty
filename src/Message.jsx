9df1c6730d3fb8bf820a6c2fb29e9e4128b457af
David Grant <mrdavidgrant@outlook.com>
2017-10-04 17:43:59 -0400

    images rendering and centered

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
+  
+
   render() {
+    const regex = new RegExp(/\w+\S+(\.png|\.jpg|\.gif)\b/)
     switch(this.props.message.type) {
       case 'incomingMessage':
+      console.log(regex.exec(this.props.message.content))
+      if(regex.exec(this.props.message.content)) {
+        var image = regex.exec(this.props.message.content)
+        var content = (this.props.message.content).replace(image[0], '')
+        return (
+          <div className="message" name={this.props.message.uuid}>
+            <span className="message-username" style={{color: this.props.message.color}}>{this.props.message.username}</span>
+            <span className="message-content">{content}</span>
+            <div className='message-image-div'>
+              <img src={image[0]} className='message-image' />
+            </div>
+          </div>
+        )
+        break
+      } else {
         return (
           <div className="message" name={this.props.message.uuid}>
           <span className="message-username" style={{color: this.props.message.color}}>{this.props.message.username}</span>
@@ -11,6 +29,7 @@ class Message extends Component {
         </div>
         )
         break
+      }
       case 'incomingNotification':
         return (
           <div className="message system">
