6b5fb4b609f866a37cf2bb19723eb10e47a7d3dd
David Grant <mrdavidgrant@outlook.com>
2017-10-04 18:09:25 -0400

    included server in git repo

---
+++ server/server.js
@@ -0,0 +1,91 @@
+// server.js
+const randomColor = require('randomcolor')
+const express = require('express');
+const SocketServer = require('ws').Server;
+const uuid4 = require ('uuid/v4')
+
+// Set the port to 3001
+const PORT = 3001;
+
+
+// Create a new express server
+const server = express()
+// Make the express server serve static assets (html, javascript, css) from the /public folder
+.use(express.static('public'))
+.listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+let messageBuffer = []
+let clientCount = 0
+let userList = {}
+
+wss.on('connection', (client) => {
+  newConnection(client)
+  
+  client.onmessage = (event) => {
+    let msg = JSON.parse(event.data)
+    msg.uuid = uuid4()
+    switch(msg.type) {
+      case "postMessage":
+        msg.type = 'incomingMessage'
+        msg.color = userList[msg.userid].color
+        break
+      case "postNotification":
+        msg.type = 'incomingNotification'
+        msg.color = userList[msg.userid].color
+        break
+      default:
+        console.log(msg.type)
+    }
+    bufferMessage(msg)
+    broadcast(msg)
+  }
+  client.on('close', () => {
+    console.log('Client disconnected')
+    clientCount--
+    sendCount(clientCount)
+  });
+});
+
+function bufferMessage(msg) {
+  if (messageBuffer.length <= 10) {
+    messageBuffer.push(msg)
+  } else {
+    messageBuffer.shift()
+    messageBuffer.push(msg)
+  }
+}
+
+function newConnection (client) {
+  console.log("Client connected")
+  clientCount ++
+  sendCount (clientCount)
+  if (messageBuffer.length){
+    client.send(JSON.stringify(messageBuffer))
+  }
+  let user = {
+    userid: uuid4(),
+    color: randomColor()
+  }
+  let content = {
+    type: 'incomingUser',
+    content: user
+  }
+  userList[user.userid] = user
+  client.send(JSON.stringify(content))
+}
+  
+function broadcast(msg) {
+  wss.clients.forEach(function each(client) {
+    client.send(JSON.stringify(msg))
+  })
+}
+        
+function sendCount (clientCount) {
+  let msg = {
+    type: 'incomingCount',
+    content: clientCount,
+    uuid: uuid4()
+  }
+  broadcast(msg)
+}
